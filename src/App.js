2561e38895d6b11cc705391934a6d1dfa28d4dcb
Prince Kunal <princekunal652@gmail.com>
2024-02-06 01:15:07 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Mohe Mohe the learning platform with Mind Map.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://mohemohe.in"
           target="_blank"
           rel="noopener noreferrer"
         >
